fffa5fa0ac31f2550dcb27bad01a3baf7194e367
sjohns2020 <98856218+sjohns2020@users.noreply.github.com>
2022-05-22 17:37:39 +0100

    second commit

--- models/park.js
+++ models/park.js
@@ -4,90 +4,92 @@ const Park = function (name, ticketPrice, dinosaurs = []) {
     this.dinosaurs = dinosaurs;
 }
   
-  module.exports = Park;
 
-  Park.prototype.numberOfDinosaurs = function () {
-      return this.dinosaurs.length
-  }
+Park.prototype.numberOfDinosaurs = function () {
+    return this.dinosaurs.length
+}
 
-  Park.prototype.addDinosaur = function (dinosaur) {
-      this.dinosaurs.push(dinosaur)
-  }
+Park.prototype.addDinosaur = function (dinosaur) {
+    this.dinosaurs.push(dinosaur)
+}
 
-  Park.prototype.removeDinosaur = function (dinosaur) {
-  const indexOfDinosaur = this.dinosaurs.indexOf(dinosaur)
-  this.dinosaurs.splice(indexOfDinosaur, 1)
-  }
+Park.prototype.removeDinosaur = function (dinosaur) {
+    const indexOfDinosaur = this.dinosaurs.indexOf(dinosaur)
+    this.dinosaurs.splice(indexOfDinosaur, 1)
+}
 
 //   just giving me back the dinosuar with index 0
-  Park.prototype.dinosaurWithMostVisitors = function () {
+Park.prototype.dinosaurWithMostVisitors = function () {
     const mostPopularDino = this.dinosaurs[0]
     for (dinosaur of this.dinosaurs) {
         if (this.dinosaurs.guestsAttractedPerDay > mostPopularDino.guestsAttractedPerDay) {
             mostPopularDino = this.dinosaur
         }
-        }
+    }
     return mostPopularDino
-    }
+}
 
-    // show a list of dinosaurs by sepcies
-    Park.prototype.searchBySpecies = function(species) {
-        const matchingSpecies = [];
-        for (dinosaur of this.dinosaurs) { 
-            if (dinosaur.species === species) {
-                matchingSpecies.push(dinosaur);
-            }
+// show a list of dinosaurs by sepcies
+Park.prototype.searchBySpecies = function(species) {
+    const matchingSpecies = [];
+    for (dinosaur of this.dinosaurs) { 
+        if (dinosaur.species === species) {
+            matchingSpecies.push(dinosaur);
         }
-        return matchingSpecies
     }
+    return matchingSpecies
+}
 // adding total visitors
-    Park.prototype.totalVisitors = function() {
-        let total = 0;
-        for (dinosaur of this.dinosaurs) { 
-            total += dinosaur.guestsAttractedPerDay;
-            }
-            return total
-            }
+Park.prototype.totalVisitors = function() {
+    let total = 0;
+    for (dinosaur of this.dinosaurs) { 
+        total += dinosaur.guestsAttractedPerDay;
+    }
+    return total
+}
 
-    Park.prototype.totalVisitorsPerYear = function() {
-        let total = 0;
-        for (dinosaur of this.dinosaurs) { 
-            total += (dinosaur.guestsAttractedPerDay * 365);
-            }
-            return total
-            }
+Park.prototype.totalVisitorsPerYear = function() {
+    let total = 0;
+    for (dinosaur of this.dinosaurs) { 
+        total += (dinosaur.guestsAttractedPerDay * 365);
+    }
+    return total
+}
 
-    Park.prototype.totalAnnualRevenue = function() {
-        let result = 0;
-        let total = 0
-        for (dinosaur of this.dinosaurs) { 
-            total += (dinosaur.guestsAttractedPerDay * 365);
-            result = total * this.ticketPrice
-            }
-            return result
-            }
-    
+Park.prototype.totalAnnualRevenue = function() {
+    let result = 0;
+    let total = 0
+    for (dinosaur of this.dinosaurs) { 
+        total += (dinosaur.guestsAttractedPerDay * 365);
+        result = total * this.ticketPrice
+    }
+    return result
+}
 
-    Park.prototype.removeDinosaurWithSpecies = function(species) {
-        let matchingSpecies = []; 
-        let newList = []
-        for (dinosaur of this.dinosaurs) { 
-            if (dinosaur.species === species) {
-                matchingSpecies.push(dinosaur);
-                newList = this.dinosaurs.splice(0, matchingSpecies.length)
-                }
-            }
-            return newList
+
+Park.prototype.removeDinosaurWithSpecies = function(species) {
+    let matchingSpecies = []; 
+    let newList = []
+    for (dinosaur of this.dinosaurs) { 
+        if (dinosaur.species === species) {
+            matchingSpecies.push(dinosaur);
+            newList = this.dinosaurs.splice(0, matchingSpecies.length)
         }
+    }
+    return newList
+}
 
-    Park.prototype.typesAndNumberOfDiets = function() {
-        const counts = {};
-        const typesOfDiet = [];
-        for (dinosaur of this.dinosaurs) {
+Park.prototype.typesAndNumberOfDiets = function() {
+    const counts = {};
+    const typesOfDiet = [];
+    for (dinosaur of this.dinosaurs) {
         typesOfDiet.push(dinosaur.diet);
         typesOfDiet.forEach = function (x) { 
             counts[x] = (counts[x] || 0) + 1; };
         }
         return counts
     }
-        
+    module.exports = Park;
+    
+    
+    
